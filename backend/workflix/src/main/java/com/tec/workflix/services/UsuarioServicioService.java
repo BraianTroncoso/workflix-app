e3ede809b429c8c21880ebd7cc156834cccc7e0e
Braian <troncosobraianaxel@gmail.com>
2023-11-14 02:58:13 -0300

    [ADD] UsuarioServicioService - inyeccion dao

---
+++ backend/workflix/src/main/java/com/tec/workflix/services/UsuarioServicioService.java
@@ -0,0 +1,21 @@
+package com.tec.workflix.services;
+
+import com.tec.workflix.interfaces.IUsuarioServicioInterface;
+import com.tec.workflix.modelsDAO.UsuarioServicioDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UsuarioServicioService implements IUsuarioServicioInterface {
+
+    @Autowired
+    UsuarioServicioDAO dao;
+
+    @Override
+    public List<Map<String, Object>> listar() {
+        return dao.listar();
+    }
+}
