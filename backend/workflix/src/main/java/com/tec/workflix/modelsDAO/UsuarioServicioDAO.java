2f4e30e5b437cded0d0b75727756f46198275e6e
Braian <troncosobraianaxel@gmail.com>
2023-11-14 02:55:15 -0300

    [ADD] UsuarioServicioDAO - query

--- backend/workflix/src/main/java/com/tec/workflix/modelsDAO/UsuarioServicioDAO.java
+++ backend/workflix/src/main/java/com/tec/workflix/modelsDAO/UsuarioServicioDAO.java
@@ -1,5 +1,23 @@
 package com.tec.workflix.modelsDAO;
 
-public class UsuarioServicioDAO {
+import com.tec.workflix.interfaces.IUsuarioServicioInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UsuarioServicioDAO implements IUsuarioServicioInterface {
+
+    @Autowired
+    JdbcTemplate template;
+
+    @Override
+    public List<Map<String, Object>> listar() {
+        List<Map<String, Object>> list = template.queryForList("select * from usuario_servicio");
+        return list;
+    }
 
 }
