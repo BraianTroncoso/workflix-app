572e9a561e67c24fd70f4685138c3415c45a4f30
Braian <troncosobraianaxel@gmail.com>
2023-11-14 02:53:06 -0300

    [ADD] listar metodo - Interface

--- backend/workflix/src/main/java/com/tec/workflix/interfaces/IUsuarioServicioDAO.java
+++
@@ -1,4 +0,0 @@
-package com.tec.workflix.interfaces;
-
-public class IUsuarioServicioDAO {
-}
---
+++ backend/workflix/src/main/java/com/tec/workflix/interfaces/IUsuarioServicioInterface.java
@@ -0,0 +1,9 @@
+package com.tec.workflix.interfaces;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IUsuarioServicioInterface {
+
+    public List<Map<String, Object>> listar();
+}
