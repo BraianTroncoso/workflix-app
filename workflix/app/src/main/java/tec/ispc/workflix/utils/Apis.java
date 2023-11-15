d9db3006f3dc8bee0ba4db865fde88c861e0deeb
Braian <troncosobraianaxel@gmail.com>
2023-11-14 23:35:11 -0300

    [ADD] interface - UsuarioServicio - servicios

--- workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
@@ -6,7 +6,7 @@ public class Apis {
     public static final String URL_001="http://192.168.0.125:8080/servicios/";
     public static final String URL_002="http://192.168.0.125:8080/usuarios/";
 
-    public static final String URL_003="http://192.168.0.125:8080/preferencias/";
+    public static final String URL_003="http://192.168.0.125:8080/prestaciones/";
 
 
 
